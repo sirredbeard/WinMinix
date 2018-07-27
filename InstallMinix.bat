031c0246ed465d0e051e274032579a60255ed77a
Hayden <33820650+sirredbeard@users.noreply.github.com>
2018-07-27 19:40:04 -0400

    Added Virtual HD creation

--- InstallMinix.bat
+++ InstallMinix.bat
@@ -1,1 +1,2 @@
+Dependencies\qemu\qemu-img
 Dependencies\qemu\qemu-system-x86_64.exe -hda mydisk.img -cdrom minix_R3.4.0rc6.iso -m 2048M -net user -net nic -boot d -display curses -cpu Skylake-Client
