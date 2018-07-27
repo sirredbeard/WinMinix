f555983d0efa3acde23774c5ffbb4c4cab910024
Hayden <33820650+sirredbeard@users.noreply.github.com>
2018-07-27 19:42:16 -0400

    Added MyDisk.img creation to Install

--- InstallMinix.bat
+++ InstallMinix.bat
@@ -1,2 +1,2 @@
-Dependencies\qemu\qemu-img
+Dependencies\qemu\qemu-img.exe create -f qcow2 mydisk.img 4G
 Dependencies\qemu\qemu-system-x86_64.exe -hda mydisk.img -cdrom minix_R3.4.0rc6.iso -m 2048M -net user -net nic -boot d -display curses -cpu Skylake-Client
